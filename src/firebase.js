cef75de3d222f77e6ff7b810c8623208d2cf0dcc
grimenglish <grimenglish@naver.com>
2025-05-08 23:57:32 +0900

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOilDrnpuaTuu0U8YaEnJou2viYQKwMLE",
+  authDomain: "grimenglish1.firebaseapp.com",
+  projectId: "grimenglish1",
+  storageBucket: "grimenglish1.firebasestorage.app",
+  messagingSenderId: "324834382601",
+  appId: "1:324834382601:web:51d1158ca1f956d8c3f9d8",
+  measurementId: "G-J9D8BH0HN6"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
